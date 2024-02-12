15da53c4cfeab498e6f3d8bea4dcf5bbec7b25d0
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-02-12 18:18:35 +0100

    edit name AppVue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
 export default {
-  name: 'appvue',
+  name: 'AppVue',
   components: { AppHeader, AppMain, AppFooter }
 }
 </script>
