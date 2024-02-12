118d80cc3d5b403d12f10e610b6689c52d51d442
Polvere243 <danielercoli@gmail.com>
2024-02-13 00:07:20 +0100

    did most of the section, but can't import icon

--- src/components/SectionVideo.vue
+++ src/components/SectionVideo.vue
@@ -5,9 +5,59 @@ export default {
 </script>
 
 <template>
-    <!-- Template Here -->
+    <section id="jazz">
+        <h5>WHO WE ARE</h5>
+        <h2>SENSE THE JAZZ</h2>
+        <p>The Story Of Jazz And Blues is the The Story Of Determination, Passion, Idealism, Community, And Of Course,
+        Music. Founded In 1985 And Incorporated In 1986, As A Community Based, Not-Fro-Profit, Charitable Arts
+        Organization Located In Vancouver, British Columbia, The Society, From The Very Beginning, Has Worked Diligently
+        To Establish Vancouver As A Centre For The Creation And Exchange Of Sounds And Ideas Between The Local,
+        National And International Music Communities</p>
+        <button>READ MORE</button>
+    </section>
+    <section id="video">
+        <!-- <font-awesome-icon :icon="['far', 'circle-play']" /> -->
+        <h2>NEW MUSIC VIDEO</h2>
+    </section>    
 </template>
 
 <style lang="scss" scoped >
-/* Style here */
+
+#jazz{
+    height: 500px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-width: 660px;
+    margin: 0 auto;
+}
+
+ h5{
+    color: #f2870c;
+    text-align: center;
+ }
+
+ h2, 
+ p {
+    color: #ffffff;
+    text-align: center;
+ }
+
+
+
+ button{
+    padding: 10px 50px;
+    background-color: transparent;
+    border: 1px solid #f2870c;
+    color: #ffffff;
+    margin-top: 30px;
+ }
+
+ #video{
+    height: 600px;
+    background-image: url(../assets/img/Video-600x211.jpg);
+    background-repeat: no-repeat;
+    background-size: contain;
+ }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '@fontsource-variable/oswald';
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCirclePlay } from '@fortawesome/free-regular-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faCirclePlay);
+
+
+const app = createApp(App);
+app.component('FontAwesomeIcon', FontAwesomeIcon);
+app.mount('#app');
