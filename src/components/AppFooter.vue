6493dfeb15b2dd5776dcb69b70d8a1256b8d141c
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-02-12 18:52:57 +0100

    add footer copyright credit

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,38 @@
 <script>
+import { footerAddresses, footerInfo, footerSupport } from '@/assets/data/data.js';
+import FooterRights from './footer-components/FooterRights.vue';
+
 export default {
     name: "AppFooter",
+    components: { FooterRights, },
+    data() {
+        return {
+            footerAddresses: footerAddresses,
+            footerInfo: footerInfo,
+            footerSupport: footerSupport
+        };
+    }
 };
 </script>
 
 <template>
-    <!-- Template Here -->
+    <div id="footer-container">
+        <div id="footer-menus"></div>
+        <FooterRights />
+    </div>
 </template>
 
-<style lang="scss" scoped >
-/* Style here */
+<style scoped >
+#footer-container {
+    max-width: 1400px;
+    margin: 0 auto;
+}
+
+
+
+#footer-menus {
+    min-height: 300px;
+    border: 2px dotted white;
+
+}
 </style>
---
+++ src/components/footer-components/FooterRights.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'FooterRights',
+};
+</script>
+
+<template>
+    <div id="footer-rights">
+        <p>© 2024 All Rights Reserved. Developed By TemplateMela</p>
+    </div>
+</template>
+
+
+<style scoped>
+#footer-rights {
+    padding: 15px 0 15px 0;
+    border-top: 1px solid gray;
+    border-bottom: 1px solid gray;
+
+    color: gray;
+    text-align: center;
+    line-height: 3rem;
+    font-size: 1rem;
+    font-weight: 400;
+    letter-spacing: 2px;
+}
+</style>
