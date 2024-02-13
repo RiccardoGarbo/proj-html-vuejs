d8952b2deda43b1a997edf4f16afcc1632779182
ariannacostanzo <costanzo.arianna@outlook.it>
2024-02-13 14:04:51 +0100

    implementing headernavbar

--- src/components/header-components/HeaderNavbar.vue
+++ src/components/header-components/HeaderNavbar.vue
@@ -1,13 +1,48 @@
 <script>
+import { headerNav} from '../../assets/data/data'
   export default {
-    name: 'HeaderNavbar'
+    name: 'HeaderNavbar',
+    data() {
+        return {
+            headerNav
+        }
+    },
   }
 </script>
 
 <template>
-  <!-- template here -->
+  <nav>
+        <ul>
+            <li v-for="(link, i) in headerNav" :key="i">
+                <a :href="link.url" :class="{ active: link.active }">{{
+                    link.title }}
+                </a>
+            </li>
+        </ul>
+    </nav>
 </template>
 
 <style lang='scss' scoped>
-  /* style here */
+@use '../../assets/scss/vars' as *;
+
+  nav ul {
+        display: flex;
+        align-items: center;
+        gap: 45px;
+        list-style-type: none;
+
+
+        .active {
+            color: $orange-cs;
+        }
+    }
+
+    nav a {
+        font-size: 15px;
+        text-decoration: none;
+        font-weight: 700;
+        line-height: 24px;
+        cursor: pointer;
+        color: white;
+    }
 </style>
