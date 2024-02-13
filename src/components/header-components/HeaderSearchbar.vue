1fc9ccda1c3803ab4bef714034b6d72afdd3631c
ariannacostanzo <costanzo.arianna@outlook.it>
2024-02-13 14:04:38 +0100

    implementing searchbar component

---
+++ src/components/header-components/HeaderSearchbar.vue
@@ -0,0 +1,17 @@
+<script>
+  export default {
+    name: 'HeaderSearchbar'
+  }
+</script>
+
+<template>
+  <div id="search-icon"><img src="../../assets/img/image (8).svg" alt="lente"></div>
+</template>
+
+<style lang='scss' scoped>
+  #search-icon {
+        filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(304deg) brightness(102%) contrast(103%);
+        padding: 0 1rem;
+        cursor: pointer;
+    }
+</style>
