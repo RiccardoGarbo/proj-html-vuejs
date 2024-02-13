ed106fef1b602069f21a0ffe0d5c5ddda674ec71
ariannacostanzo <costanzo.arianna@outlook.it>
2024-02-13 14:05:00 +0100

    implementing headerlogo

---
+++ src/components/header-components/HeaderLogo.vue
@@ -0,0 +1,15 @@
+<script>
+  export default {
+    name: 'HeaderLogo'
+  }
+</script>
+
+<template>
+  <figure>
+        <img src="../../assets/img/Logo.png" alt="logo">
+    </figure>
+</template>
+
+<style lang='scss' scoped>
+  /* style here */
+</style>
