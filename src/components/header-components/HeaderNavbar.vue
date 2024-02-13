0be90c4e47b94f43bcfb5b78eb70bd6d64174110
ariannacostanzo <costanzo.arianna@outlook.it>
2024-02-13 13:25:26 +0100

    implementing components

---
+++ src/components/header-components/HeaderNavbar.vue
@@ -0,0 +1,13 @@
+<script>
+  export default {
+    name: 'HeaderNavbar'
+  }
+</script>
+
+<template>
+  <!-- template here -->
+</template>
+
+<style lang='scss' scoped>
+  /* style here */
+</style>
